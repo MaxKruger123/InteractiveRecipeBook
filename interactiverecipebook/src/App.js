0324bbec70bdf54ffe2a43eec7d73797c62e337d
MaxKruger123 <maxkrugeradept@gmail.com>
2024-04-13 14:07:28 +0200

    Centred items

--- interactiverecipebook/src/App.js
+++ interactiverecipebook/src/App.js
@@ -54,24 +54,49 @@ import RecipeDropdown from './recipeDropdown';
     dietaryRestrictions: 'contains milk'
   };
 
+
+  const AllRecipesButton = ({ onClick }) => {
+    return (
+      <button onClick={onClick}>Show All Recipes</button>
+    );
+  };
+
+
 //App component
 const App = () => {
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [showAllRecipes, setShowAllRecipes] = useState(false);
 
   const handleSelectRecipe = (recipeName) => {
     const recipe = recipes.find(recipe => recipe.name === recipeName);
     setSelectedRecipe(recipe);
+    setShowAllRecipes(false);
+  };
+
+  const handleShowAllRecipes = () => {
+    setShowAllRecipes(true);
+    setSelectedRecipe(null);
   };
 
   const recipes = [pizzaRecipe, chickenKormaRecipe, omelleteRecipe];
-  
+
   return (
-    <div>
+    <div className="container">
       <h1>Recipe Book</h1>
       {/* Render the RecipeDropdown component and pass the recipes array and onSelectRecipe function */}
       <RecipeDropdown recipes={recipes} onSelectRecipe={handleSelectRecipe} />
-      {/* Render the Recipe component with the selected recipe details */}
+      {/* Render the AllRecipesButton component and pass the onClick function */}
+      <AllRecipesButton onClick={handleShowAllRecipes} />
+      {/* Render the Recipe component with the selected recipe details if selectedRecipe is not null */}
       {selectedRecipe && <Recipe recipe={selectedRecipe} />}
+      {/* Render all recipes if showAllRecipes is true */}
+      {showAllRecipes && (
+        <div>
+          {recipes.map((recipe, index) => (
+            <Recipe key={index} recipe={recipe} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
